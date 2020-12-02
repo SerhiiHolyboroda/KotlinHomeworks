04eddc489929287c832a4d86b6314192a2d04c6d
Serhii Holyboroda <49484600+SerhiiHolyboroda@users.noreply.github.com>
2020-12-02 14:58:16 +0200

    Initial commit

---
+++ src/HomeTask1.kts
@@ -0,0 +1,9 @@
+import kotlin.random.Random
+
+fun main() {
+     println("Hello, Kotlin/Native!");
+    val nextValues = List(10) { Random.nextInt(0, 100) }
+    println(nextValues)
+    when(item.isOdd() -> println(it)
+    println(it)
+}
---
+++ src/nativeMain/kotlin/main.kt
@@ -0,0 +1,8 @@
+import kotlin.random.Random
+
+fun main() {
+//    println("Hello, Kotlin/Native!");
+    val nextValues = List(10) { Random.nextInt(0, 100) }
+    println(nextValues)
+
+}
