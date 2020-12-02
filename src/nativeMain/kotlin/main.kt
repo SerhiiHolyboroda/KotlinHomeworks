eeeb78576f5a2e1ad49ad4db6f11634ef3ffd8a3
Serhii Holyboroda <49484600+SerhiiHolyboroda@users.noreply.github.com>
2020-12-02 15:00:21 +0200

    Update main.kt

--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -1,8 +1,66 @@
 import kotlin.random.Random
-
 fun main() {
-//    println("Hello, Kotlin/Native!");
-    val nextValues = List(10) { Random.nextInt(0, 100) }
-    println(nextValues)
 
+val ListOfValues = List(100) { Random.nextInt(0, 100) };
+println(ListOfValues);
+
+
+fun check(Item: Int){
+    for (item in ListOfValues) {
+       when {
+            (item % 3 == 0) && (item % 5 == 0) -> println("Piff & Paff")
+            item % 3 == 0 -> println("Piff")
+            item % 5 == 0 -> println("Paff")
+
+        }
+}
+
+    }
+
+
+
+    for (item: Int in ListOfValues) {
+        when {
+            (item % 3 == 0) && (item % 5 == 0) -> println("Piff & Paff")
+            item % 3 == 0 -> println("Piff")
+            item % 5 == 0 -> println("Paff")
+
+        }
+    }
+
+     for(item in ListOfValues.indices){
+      when{
+          (item % 3 == 0) && (item % 5 == 0) -> println("Piff & Paff")
+          item % 3 == 0-> println("Piff")
+      item % 5 == 0-> println("Paff")
+  }}
+
+
+    for ((index, item) in ListOfValues.withIndex()) {
+
+        when {
+            (item % 3 == 0) && (item % 5 == 0) -> println("Piff & Paff")
+            item % 3 == 0 -> println("Piff")
+            item % 5 == 0 -> println("Paff")
+
+        }
+    }
+
+    for (item: Int in ListOfValues) {
+
+        check(item);
+    }
+
+    for(item in ListOfValues.indices){
+        check(item);
+    }
+
+    for(item in ListOfValues.indices) {
+        check(item);
+    }
+
+    for ((index, item) in ListOfValues.withIndex()) {
+        check(item);
+
+    }
 }
